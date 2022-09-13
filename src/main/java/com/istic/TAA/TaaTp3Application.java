e177606f8c158a5134cca47c72d522dc3e72a489
DylanDupond <neorockdevil@hotmail.fr>
2022-09-13 10:55:04 +0200

    initial commit

---
+++ src/main/java/com/istic/TAA/TaaTp3Application.java
@@ -0,0 +1,13 @@
+package com.istic.TAA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaaTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaaTp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/istic/TAA/TaaTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.istic.TAA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaaTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
